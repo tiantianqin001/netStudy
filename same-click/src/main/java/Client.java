3190e663b6c8bd699a31a4d91ad6bd97cda8a540
tiantianqin <qin15156589949@gmail.com>
2021-06-26 13:57:43 +0800

    提交到服务器上

--- same-click/src/main/java/Client.java
+++ same-click/src/main/java/Client.java
@@ -5,5 +5,8 @@ public class Client {
         //搜索服务端的ip 地址 然后去链接
         ServerInfo info = UDPSearcher.searchServer(10000);
         System.out.println("获取到服务端信息"+info);
+        if (info!=null){
+
+        }
     }
 }
